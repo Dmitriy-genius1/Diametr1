a267531e17b91c27bff6f078934306438565cb2b
Dmitriy <dmitriy.int560@gmail.com>
2024-02-02 02:48:51 +0600

    Box

---
+++ Message_Box/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
+					LPSTR lpCmdLine, int nShowCmd)
+
+		{
+			MessageBox(NULL, " Helo Helo Helo Helo Helo , Win32 world!",
+						"Message Box",MB_OK);
+
+		return 0;
+		}
+
+
